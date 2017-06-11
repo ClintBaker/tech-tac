2bf8fe21a7c199e5a83317cfc58236d752dc6ae2
Clint Baker <clintonjamesbaker@gmail.com>
2017-06-11 16:21:32 -0600

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/tech-tac');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/tech-tac');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Part} = require('./models/part');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -51,8 +52,8 @@ app.get('/parts/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
