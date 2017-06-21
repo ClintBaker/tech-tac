9891048b3686bcfc37cce90ccaed445a058cfa2c
Clint Baker <clintonjamesbaker@gmail.com>
2017-06-21 03:47:03 -0600

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+var bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose');
 var {Part} = require('./models/part');
@@ -122,6 +123,21 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+//POST /users/login
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+
 });
 
 
