f7493318fb35013667363c3d23f0f2b6951f9f72
Clint Baker <clintonjamesbaker@gmail.com>
2017-06-21 13:40:39 -0600

    Add DELETE /users/me/token route

---
+++ server/models/order.js
@@ -0,0 +1,16 @@
+// {
+//   _id: 'a;lsdfjwofjsadlkfj',
+//   order_number: '12353245',
+//   order_date: 'asdf23sdfjka',
+//   status: 'not shipped',
+//   order_items: [
+//     {
+//       id: '23498032458',
+//       quantity: 3
+//     },
+//     {
+//       id: '123098sdal;fj',
+//       quantity: 1
+//     }
+//   ]
+// }
