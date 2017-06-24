d6355c95e75dcea4e94a6db96373bca212e1cdbd
Clint Baker <clintonjamesbaker@gmail.com>
2017-06-24 11:02:45 -0600

    Messing with cors

--- server/server.js
+++ server/server.js
@@ -159,7 +159,7 @@ app.post('/users/login', (req, res) => {
 });
 
 //DELETE /users/me/token
-app.delete('/users/me/token', authenticate, cors(), (req, res) => {
+app.delete('/users/me/token', authenticate, (req, res) => {
   req.user.removeToken(req.token).then(() => {
     res.status(200).send();
   }, () => {
