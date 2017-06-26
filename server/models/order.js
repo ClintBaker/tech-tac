b12db7b6b67fc444aa4983276780e3a81e419c17
Clint Baker <clintonjamesbaker@gmail.com>
2017-06-26 02:04:01 -0600

    Fix issue with order validation

--- server/models/order.js
+++ server/models/order.js
@@ -7,9 +7,12 @@ var Order = mongoose.model('Orders', {
     default: 'pending'
   },
   parts: [{
+    _partId: mongoose.Schema.Types.ObjectId,
+    quantity: Number
   }],
   _companyId: {
-    required: true
+    required: true,
+    type: mongoose.Schema.Types.ObjectId
   }
 });
 
